371f77bc3baa031db84d7c8394ba3fe53c92cc31
hieumaestro <54903623+hieumaestro@users.noreply.github.com>
2019-09-14 00:22:44 +0700

    Create index.js
    Sử dụng các kiến thức đã học trong bài khai báo các biến mô tả một người, vật nào đó mà bạn thích

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Sử dụng các kiến thức đã học trong bài khai báo các biến mô tả một người, vật nào đó mà bạn thích
+ * Ví dụ dưới đây mô tả thời tiết ngày hôm nay
+ *  var today = 'Sunday';
+ *  var isCloudy = true;
+ *  var currentTemperature = 30;
+ */
+var myName = 'Minh  Hiếu';
+var isMale = true;
+var myAge = 21;
+console.log('My name: '+ myName);
+if(isMale === true) console.log('Gender: Male');
+else console.log('Gender: Female');
+console.log('My age: '+myAge);
